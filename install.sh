b01084d190493505e33b07cd4920e6c724751af7
Gareth Evans <gareth@bryncynfelin.co.uk>
2022-10-20 22:05:29 +0100

    chore: initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
