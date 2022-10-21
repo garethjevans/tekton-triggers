dbadcc69d7c5e8bdd97a7a3d37e20532fe508928
Gareth Evans <gareth@bryncynfelin.co.uk>
2022-10-21 21:21:53 +0100

    chore: wait until resources are complete

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-ng
 sleep 60
 
 # tekton and dependencies
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml --wait
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml --wait
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml --wait
 
 # tasks
-kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml
-kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/main/task/maven/0.2/maven.yaml
+kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml --wait
+kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/main/task/maven/0.2/maven.yaml --wait
