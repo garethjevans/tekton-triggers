c3be06d808c6ae6b72be285e33a1efd3b5030780
Gareth Evans <gareth@bryncynfelin.co.uk>
2022-10-21 15:51:19 +0100

    increased timeout

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # nginx
 kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml --wait
 
+sleep 60
+
 # tekton and dependencies
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
