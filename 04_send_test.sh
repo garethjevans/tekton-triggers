eb5e58db35197c23cda738256dd733730a2fe04e
Gareth Evans <gareth@bryncynfelin.co.uk>
2022-10-25 11:12:42 +0100

    chore: specify the correct namespace

--- 04_send_test.sh
+++ 04_send_test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-IP=$(kubectl get ing webhook -oyaml | yq '.status.loadBalancer.ingress[0].ip')
+IP=$(kubectl get ingress webhook -n trigger-namespace -oyaml | yq '.status.loadBalancer.ingress[0].ip')
 
 curl -v --data "@event.json" http://${IP}.nip.io
 
